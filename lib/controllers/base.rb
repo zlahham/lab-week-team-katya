49c1011fd394621481a8bbe1d9db56ae25518530
zlahham <zlahham@gmail.com>
2015-08-27 09:43:38 +0100

    Commit before working on backend again

--- lib/controllers/base.rb
+++ lib/controllers/base.rb
@@ -7,6 +7,7 @@ module VirtualAssistant
 
       include AppHelpers
       set :views, proc { File.join(root, '..', 'views')}
+      # set :public, proc { File.join(root, '../..', 'public')}
       register Sinatra::Flash
       enable :sessions
       set :session_secret, 'super_secret'
